a8f46a08077f43fc699e4cf08d316223b57b17a9
xvxexe <63173286+xvxexe@users.noreply.github.com>
2026-04-10 11:58:52 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{padding:20}}>
+      <h1>Gestionale Multicantiere pronto</h1>
+      <p>Progetto base funzionante. Incolla qui il file completo che ti ho dato.</p>
+    </div>
+  )
+}
